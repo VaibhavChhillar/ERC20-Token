533e9fe3cb37ecc17c19608f618457877448ebdb
Vaibhav Chhillar <131567808+VaibhavChhillar@users.noreply.github.com>
2024-07-08 15:42:33 +0530

    Update abc.sol

--- abc.sol
+++ abc.sol
@@ -1,1 +1,52 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC20, Ownable(msg.sender) {
+    constructor() ERC20("Degen", "DGN") {}
+
+    // Store item structure
+    struct StoreItem {
+        string itemName;
+        uint256 price;
+    }
+
+    // Store items list
+    StoreItem[] public storeItems;
+
+    // Add store items (onlyOwner)
+    function addStoreItem(string memory itemName, uint256 price) external onlyOwner {
+        storeItems.push(StoreItem(itemName, price));
+    }
+
+    // Mint new tokens and distribute them to players as rewards. Only the owner can do this.
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Transfer tokens from the sender's account to another account.
+    function transferTokens(address to, uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount, "Don't have enough Degen Tokens");
+        _transfer(msg.sender, to, amount);
+    }
+
+    // Anyone can burn their own tokens that are no longer needed.
+    function burn(uint256 amount) public virtual {
+        _burn(msg.sender, amount);
+    }
+}
+
+contract StoreInitializer {
+    // Deploy the DegenToken contract and add examples to storeItems
+    constructor() {
+        Token token = new Token();
+
+        // Add examples to storeItems
+        token.addStoreItem("Epic Sword of Valor", 100);
+        token.addStoreItem("Mystic Potion of Power", 50);
+        token.addStoreItem("Legendary Shield of Fortitude", 200);
+    }
+}
